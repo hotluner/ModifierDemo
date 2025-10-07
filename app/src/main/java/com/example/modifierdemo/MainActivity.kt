3fc9dc5daff2c1f024ef5056bec2d566184a9761
hotluner <dimkoval1@gmail.com>
2025-10-07 12:40:10 +0700

    3. Create mymodifier

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
 import kotlin.io.encoding.Base64
 
@@ -33,8 +35,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen (modifier: Modifier = Modifier){
+    val mymodifier = modifier
+        .border(width = 2.dp, color = Color.Black)
+        .padding(all = 10.dp)
     Text(
         "Hello Compose",
+        mymodifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
